9cc65abe6c2c625d99380225ebf8fa76fbf70d02
Unity <724323162@qq.com>
2023-08-29 09:46:41 +0800

    修复

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class Main : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        LuaMgr.GetInstance().Init();
+        LuaMgr.GetInstance().DoLuaFile("Main");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# XLuaBag
+一个XLua背包
+![title](https://github.com/PaperCrown-cheng/XLuaBag/blob/master/XLua%E8%83%8C%E5%8C%85%E5%B0%81%E9%9D%A2.png "封面")
